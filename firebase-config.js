31b1174e3ec02225c7a1d2d9c030bf6245d65b59
wonjun4149 <86695424+wonjun4149@users.noreply.github.com>
2025-07-23 16:42:38 +0900

    commit

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3UCmmj5eEQldRexLDUtTfTVUu5G6H7XA",
+  authDomain: "aivle-team21.firebaseapp.com",
+  projectId: "aivle-team21",
+  storageBucket: "aivle-team21.firebasestorage.app",
+  messagingSenderId: "611370837795",
+  appId: "1:611370837795:web:f44306eaabb04f335ece96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
