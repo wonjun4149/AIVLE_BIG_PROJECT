3f1cf089bb1ef9cbb3d75321d344578e82403e92
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2025-07-30 11:04:40 +0900

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhh1Z4okTMlBnll_qVf8tijV8Z5DiS4lw",
-  authDomain: "aivle-team0721.firebaseapp.com",
-  databaseURL: "https://aivle-team0721-default-rtdb.firebaseio.com",
-  projectId: "aivle-team0721",
-  storageBucket: "aivle-team0721.firebasestorage.app",
-  messagingSenderId: "902267887946",
-  appId: "1:902267887946:web:22e6c9a70fea861955e1b3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
