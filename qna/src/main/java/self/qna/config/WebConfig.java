b0006ae2cf0f4e6f3392bd327352f58af68db4af
jeong <ywb102@naver.com>
2025-08-04 14:39:24 +0900

    CORS  허용 파일 추가

---
+++ qna/src/main/java/self/qna/config/WebConfig.java
@@ -0,0 +1,18 @@
+package self.qna.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://34.54.82.32", "http://localhost:3000") // 프론트엔드 배포 주소와 로컬 주소 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 인증 정보(쿠키, 토큰 등) 허용
+    }
+}
