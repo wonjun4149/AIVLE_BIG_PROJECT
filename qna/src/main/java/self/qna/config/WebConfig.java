c6d781f4d2afb9a21845f1d0d6bac86a81628fed
jeong <ywb102@naver.com>
2025-08-04 15:04:12 +0900

    CORS 로컬 환경에서 안나도록 수정

--- qna/src/main/java/self/qna/config/WebConfig.java
+++ qna/src/main/java/self/qna/config/WebConfig.java
@@ -1,10 +1,12 @@
 package self.qna.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("docker") // "docker" 프로필이 활성화될 때만 이 설정을 사용
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
